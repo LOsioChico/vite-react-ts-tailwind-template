8b0c8ab181ea3dafb191e5bb498c3d0f372fc3e9
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-02 15:56:59 -0500

    feat: Add pre-install script to delete gitkeep files and add features

--- config/scripts/preInstall.ts
+++ config/scripts/preInstall.ts
@@ -1,5 +1,10 @@
+import addFeatures from './addFeatures.ts';
 import deleteGitKeepFilesInSrc from './deleteGitkeepFiles.ts';
 
 void (async () => {
+  // delete gitkeep files in src
   deleteGitKeepFilesInSrc();
+
+  // add features to package.json and files to src if enabled in config/features.json
+  addFeatures();
 })();
