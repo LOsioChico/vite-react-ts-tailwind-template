ea0fef247bc83d99107f98a039176720962ede39
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-02 17:32:02 -0500

    feat: Add zustand template for managing state

---
+++ config/templates/zustand/zustand.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+
+interface Store {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
+const useStore = create<Store>((set, _get) => ({
+  count: 0,
+  increment: () => {
+    set((state) => ({ count: state.count + 1 }));
+  },
+  decrement: () => {
+    set((state) => ({ count: state.count - 1 }));
+  },
+}));
+
+export default useStore;
