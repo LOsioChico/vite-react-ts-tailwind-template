7f26a86a4264a645b5896b0c790e3f08820204a8
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-02 17:33:45 -0500

    refactor: Reorder code and add comments

--- config/scripts/preInstall.ts
+++ config/scripts/preInstall.ts
@@ -1,10 +1,18 @@
-import addFeatures from './addFeatures.ts';
+import logFeaturesStatus from '../utils/logFeaturesStatus.ts';
 import deleteGitKeepFilesInSrc from './deleteGitkeepFiles.ts';
+import addFeatures from './addFeatures.ts';
+import addConfigFiles from './addConfigFiles.ts';
 
 void (async () => {
+  // log current features status
+  logFeaturesStatus();
+
   // delete gitkeep files in src
   deleteGitKeepFilesInSrc();
 
   // add features to package.json and files to src if enabled in config/features.json
   addFeatures();
+
+  // copy template files to src
+  addConfigFiles();
 })();
