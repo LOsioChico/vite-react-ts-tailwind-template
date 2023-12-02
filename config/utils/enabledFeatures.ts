cd94bfbe70203178175b0c10565d84eb962f6ddb
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-02 16:43:15 -0500

    feat: add enabledFeatures utility function

---
+++ config/utils/enabledFeatures.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import type Features from 'config/types/features';
+import type FeatureCategories from 'config/types/featureCategory';
+import type Feature from 'config/types/feature';
+
+const enabledFeatures = () => {
+  const features = JSON.parse(
+    fs.readFileSync('./config/features.json', 'utf8'),
+  ) as Features;
+
+  const enabledFeaturesResult: Feature[] = [];
+
+  // get each category of features
+  for (const category in features) {
+    // get each feature in the category
+    for (const feature in features[category as FeatureCategories]) {
+      const featureEnabled =
+        features[category as FeatureCategories][feature as Feature];
+
+      // if the feature is enabled
+      if (featureEnabled) enabledFeaturesResult.push(feature as Feature);
+    }
+  }
+
+  return enabledFeaturesResult;
+};
+
+export default enabledFeatures;
