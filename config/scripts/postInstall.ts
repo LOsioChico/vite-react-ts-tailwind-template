51911adf14903987c99cca3910b4dc5c3a7c3cb1
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-02 19:57:09 -0500

    refactor: update postInstall script to clean up package.json

--- config/scripts/postInstall.ts
+++ config/scripts/postInstall.ts
@@ -7,10 +7,15 @@ void (async () => {
   if (config.postinstall.deleteConfigFolder) {
     // delete the config folder and package.json script
     fs.rmSync('./config', { recursive: true });
+  }
 
+  if (config.postinstall.cleanPackageJson) {
     const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+
     delete packageJson.scripts.preinstall;
     delete packageJson.scripts.postinstall;
+    delete packageJson.devDependencies['@types/node'];
+
     fs.writeFileSync(
       './package.json',
       JSON.stringify(packageJson, null, 2) + '\n',
