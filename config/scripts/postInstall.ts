e4d127907eaefbc557322f5f49d44a1258435ea0
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-03 00:10:27 -0500

    refactor: Simplify postInstall and preInstall scripts

--- config/scripts/postInstall.ts
+++ config/scripts/postInstall.ts
@@ -1,30 +1,39 @@
 import fs from 'fs';
 import { exec } from 'child_process';
+import deleteGitKeepFilesInSrc from './deleteGitkeepFiles.ts';
+import addConfigFiles from './addConfigFiles.ts';
+import addFeatures from './addFeatures.ts';
 
 const config = JSON.parse(fs.readFileSync('./config/config.json', 'utf-8'));
 
 void (async () => {
-  if (config.postinstall.deleteConfigFolder) {
-    // delete the config folder and package.json script
-    fs.rmSync('./config', { recursive: true });
-  }
+  // remove dependencies
+  exec('pnpm remove @types/node');
 
-  if (config.postinstall.cleanPackageJson) {
-    const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+  // add features to package.json and files to src if enabled in config/features.json
+  addFeatures();
 
-    delete packageJson.scripts.preinstall;
-    delete packageJson.scripts.postinstall;
+  // delete gitkeep files in src
+  if (config.deleteGitkeepFiles) deleteGitKeepFilesInSrc();
 
-    fs.writeFileSync(
-      './package.json',
-      JSON.stringify(packageJson, null, 2) + '\n',
-    );
+  // copy template files to src
+  if (config.addTemplateConfigFiles) addConfigFiles();
 
-    // remove dependencies
-    exec('pnpm remove @types/node');
-  }
+  // delete the preinstall and postinstall scripts from package.json
+  const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-  if (config.postinstall.commitChanges) {
+  delete packageJson.scripts['pnpm:devPreinstall'];
+  delete packageJson.scripts.postinstall;
+
+  fs.writeFileSync(
+    './package.json',
+    JSON.stringify(packageJson, null, 2) + '\n',
+  );
+
+  // delete the config folder and package.json script
+  fs.rmSync('./config', { recursive: true });
+
+  if (config.commitChanges) {
     // commit changes
     exec('git add .');
     exec(
--- config/scripts/preInstall.ts
+++ config/scripts/preInstall.ts
@@ -1,21 +1,6 @@
-import fs from 'fs';
 import logFeaturesStatus from '../utils/logFeaturesStatus.ts';
-import deleteGitKeepFilesInSrc from './deleteGitkeepFiles.ts';
-import addFeatures from './addFeatures.ts';
-import addConfigFiles from './addConfigFiles.ts';
-
-const config = JSON.parse(fs.readFileSync('./config/config.json', 'utf-8'));
 
 void (async () => {
   // log current features status
   logFeaturesStatus();
-
-  // delete gitkeep files in src
-  if (config.preinstall.deleteGitkeepFiles) deleteGitKeepFilesInSrc();
-
-  // add features to package.json and files to src if enabled in config/features.json
-  addFeatures();
-
-  // copy template files to src
-  if (config.preinstall.addTemplateConfigFiles) addConfigFiles();
 })();
