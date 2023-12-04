467640784a7bde5712d1924c6e0f8f115d549def
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-03 19:33:09 -0500

    refactor: Update postInstall script to use fs/promises and clack prompts

--- config/scripts/postInstall.ts
+++ config/scripts/postInstall.ts
@@ -1,29 +1,64 @@
-import fs from 'fs';
+import fs from 'fs/promises';
+import * as clack from '@clack/prompts';
 import { exec } from 'child_process';
 import deleteGitKeepFilesInSrc from './deleteGitkeepFiles.ts';
 import addConfigFiles from './addConfigFiles.ts';
 import addFeatures from './addFeatures.ts';
-import logFeaturesStatus from '../utils/logFeaturesStatus.ts';
+import clackConfigureMenu from '../utils/clackConfigureMenu.ts';
+import clackAddAdditionalFeaturesMenu from '../utils/clackAddAdditionalFeaturesMenu.ts';
 // import cleanProyect from '../utils/cleanProyect.ts';
 
-const config = JSON.parse(fs.readFileSync('./config/config.json', 'utf-8'));
+const configJson = JSON.parse(
+  await fs.readFile('./config/config.json', { encoding: 'utf-8' }),
+);
 
 void (async () => {
-  await logFeaturesStatus();
+  clack.intro(
+    '\x1b[46m\x1b[30m' + ' Configure the initial proyect ' + '\x1b[0m',
+  );
+  clack.note(`The config folder will be removed after the configuration
+and the package.json file will be cleaned.`);
+
+  // configure the initial proyect
+  const shouldConfigure = await clack.confirm({
+    message: 'Do you want to modify the default configuration?',
+  });
+
+  if (clack.isCancel(shouldConfigure)) {
+    clack.outro('Operation cancelled.');
+    process.exit(0);
+  }
+
+  if (shouldConfigure) await clackConfigureMenu();
+
+  // add additional features
+  const shouldAddAdditionalFeatures = await clack.confirm({
+    message: 'Do you want to add additional features?',
+  });
+
+  if (clack.isCancel(shouldAddAdditionalFeatures)) {
+    clack.outro('Operation cancelled.');
+    process.exit(0);
+  }
+
+  if (shouldAddAdditionalFeatures) await clackAddAdditionalFeaturesMenu();
+
+  clack.outro('Configuration completed successfully.');
+  process.exit(0);
 
   // add features to package.json and files to src if enabled in config/features.json
   await addFeatures();
 
   // delete gitkeep files in src
-  if (config.deleteGitkeepFiles) await deleteGitKeepFilesInSrc();
+  if (configJson.deleteGitkeepFiles) await deleteGitKeepFilesInSrc();
 
   // copy template files to src
-  if (config.addTemplateConfigFiles) await addConfigFiles();
+  if (configJson.addTemplateConfigFiles) await addConfigFiles();
 
   // clean proyect
   // await cleanProyect();
 
-  if (config.commitChanges) {
+  if (configJson.commitChanges) {
     // commit changes
     exec('git add .');
     exec(
@@ -31,4 +66,7 @@ void (async () => {
     );
     // exec('git push');
   }
-})();
+})().catch((error) => {
+  clack.outro(`✖ ${error.message}`);
+  process.exit(1);
+});
