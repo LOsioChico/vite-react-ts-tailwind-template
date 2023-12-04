07fc18eea42992ff7d236be3c6ebcab0a1886d5f
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-03 21:06:35 -0500

    feat: Add spinner option to addDependencies function

--- config/utils/addDependencies.ts
+++ config/utils/addDependencies.ts
@@ -1,17 +1,26 @@
+import type Spinner from '../types/spinner.ts';
 import filterInstalledDependencies from './filterInstalledDependencies.ts';
 import installDependencies from './installDependencies.ts';
 
-const addDependencies = async (
+const addDependencies: AddDependencies = async (
   dependencies: string[],
-  { dev = false } = {},
+  { dev = false, spinner },
 ) => {
   if (!dependencies.length) return;
 
-  const dependenciesToInstall = await filterInstalledDependencies(dependencies);
+  const dependenciesToInstall = await filterInstalledDependencies(
+    dependencies,
+    spinner,
+  );
 
   if (dependenciesToInstall.length) {
     await installDependencies(dependenciesToInstall, { dev });
   }
 };
+
+type AddDependencies = (
+  dependencies: string[],
+  options: { dev?: boolean; spinner: Spinner },
+) => Promise<void>;
 
 export default addDependencies;
