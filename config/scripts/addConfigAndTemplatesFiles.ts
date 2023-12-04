298587127aa9b3482ebe1f17ac0ab9d81f1c176a
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-04 17:29:36 -0500

    refactor: Combine addConfigFiles and addConfigAndTemplatesFiles

--- config/scripts/addConfigAndTemplatesFiles.ts
+++ config/scripts/addConfigAndTemplatesFiles.ts
@@ -1,12 +1,17 @@
+import copyTemplatesFolders from '../utils/copyTemplatesFolders.ts';
 import copyConfigfiles from '../utils/copyConfigFiles.ts';
 import enabledFeatures from '../utils/enabledFeatures.ts';
 
-const addConfigFiles = async () => {
+const addConfigAndTemplatesFiles = async () => {
   const features = await enabledFeatures();
 
   for (const feature of features) {
     await copyConfigfiles(feature);
   }
+
+  for (const feature of features) {
+    await copyTemplatesFolders(feature);
+  }
 };
 
-export default addConfigFiles;
+export default addConfigAndTemplatesFiles;
