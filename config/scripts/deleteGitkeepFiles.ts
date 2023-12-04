9bc6a35af0710616f124f63492075341946b32c3
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-03 21:37:46 -0500

    refactor: Simplify deleteGitKeepFiles function and improve code readability

--- config/scripts/deleteGitkeepFiles.ts
+++ config/scripts/deleteGitkeepFiles.ts
@@ -5,28 +5,23 @@ import path from 'path';
 const deleteGitKeepFiles = async (directoryPath: string) => {
   const files = await fs.readdir(directoryPath);
 
-  await Promise.all(
-    files.map(async (file) => {
-      const filePath = path.join(directoryPath, file);
-      const fileStat = await fs.stat(filePath);
+  for (const file of files) {
+    const filePath = path.join(directoryPath, file);
+    const fileStat = await fs.stat(filePath);
 
-      if (fileStat.isDirectory()) {
-        void deleteGitKeepFiles(filePath);
-      } else if (file === '.gitkeep') {
-        await fs.unlink(filePath);
-        console.log(
-          `File ${file} deleted in ${directoryPath.split('\\').at(-1)}`,
-        );
-      }
-    }),
-  );
+    if (fileStat.isDirectory()) {
+      await deleteGitKeepFiles(filePath);
+    } else if (file === '.gitkeep') {
+      await fs.unlink(filePath);
+      console.log(
+        `File ${file} deleted in ${directoryPath.split('\\').at(-1)}`,
+      );
+    }
+  }
 };
 
 const deleteGitKeepFilesInSrc = async () => {
-  // we need to use this because of esm
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  const __dirname = path.dirname(new URL(import.meta.url).pathname).slice(1); // Remove initial slash
-  const directoryPath = path.join(__dirname, '../../src');
+  const directoryPath = path.join(process.cwd(), 'src');
 
   if (fsSync.existsSync(directoryPath)) {
     await deleteGitKeepFiles(directoryPath);
