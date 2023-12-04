cab18ca94d5d74bc737567d8c1c00082859b34f4
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-03 20:36:56 -0500

    refactor: Refactor installDependencies function to use async/await and handle errors

--- config/utils/installDependencies.ts
+++ config/utils/installDependencies.ts
@@ -1,6 +1,9 @@
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
 
-const installDependencies = (dependencies: string[], { dev = false } = {}) => {
+const installDependencies = async (
+  dependencies: string[],
+  { dev = false } = {},
+) => {
   if (!dependencies.length) return;
 
   console.log(`Adding ${dependencies.join(', ')}...`);
@@ -8,11 +11,21 @@ const installDependencies = (dependencies: string[], { dev = false } = {}) => {
   const args = [...dependencies];
   if (dev) args.unshift('-D');
 
-  execSync(`pnpm add ${args.join(' ')}`, {
-    stdio: 'inherit',
+  // exec(`pnpm add ${args.join(' ')}`);
+  await new Promise((resolve, reject) => {
+    exec(`pnpm add ${args.join(' ')}`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`Failed to add ${dependencies.join(', ')}.`);
+        reject(error);
+      }
+      if (stderr) {
+        console.log(`Failed to add ${dependencies.join(', ')}.`);
+        reject(stderr);
+      }
+      console.log(`Added ${dependencies.join(', ')} successfully.`);
+      resolve(stdout);
+    });
   });
-
-  console.log(`${dependencies.join(', ')} added!`);
 };
 
 export default installDependencies;
