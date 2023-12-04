b95a2af042735027dd921feeafa709db09bd5208
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-03 22:07:11 -0500

    refactor: Simplify path resolution in configFiles.ts

--- config/constants/configFiles.ts
+++ config/constants/configFiles.ts
@@ -1,17 +1,16 @@
 import type Configfiles from 'config/types/configFiles';
 import path from 'path';
 
-// we need to use this because of esm
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const __dirname = path.dirname(new URL(import.meta.url).pathname).slice(1); // Remove initial slash
+const inputBasePath = path.join(process.cwd(), 'config/templates');
+const outputBasePath = path.join(process.cwd(), 'src');
 
 const configFiles: Configfiles = {
   zustand: {
     files: [
       {
         name: 'zustand store template',
-        input: path.resolve(__dirname, '../templates/zustand/zustand.ts'),
-        output: path.resolve(__dirname, '../../src/store/zustand.ts'),
+        input: path.join(inputBasePath, 'zustand/zustand.ts'),
+        output: path.join(outputBasePath, 'store/zustand.ts'),
       },
     ],
   },
