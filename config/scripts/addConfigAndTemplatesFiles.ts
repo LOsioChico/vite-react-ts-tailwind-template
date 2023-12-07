819115ccf466524bda290398927636903fa57212
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-07 08:51:01 -0500

    feat: Add processFeatureChange function

--- config/scripts/addConfigAndTemplatesFiles.ts
+++ config/scripts/addConfigAndTemplatesFiles.ts
@@ -1,9 +1,17 @@
 import copyTemplatesFolders from '../utils/copyTemplatesFolders.ts';
 import copyConfigfiles from '../utils/copyConfigFiles.ts';
 import enabledFeatures from '../utils/enabledFeatures.ts';
+import processFeatureChange from '../utils/processFeatureChanges.ts';
+import * as clack from '@clack/prompts';
 
 const addConfigAndTemplatesFiles = async () => {
   const features = await enabledFeatures();
+
+  if (!features.length) {
+    const spinner = clack.spinner();
+    spinner.start();
+    spinner.stop('✔ No features enabled.');
+  }
 
   for (const feature of features) {
     await copyConfigfiles(feature);
@@ -11,6 +19,10 @@ const addConfigAndTemplatesFiles = async () => {
 
   for (const feature of features) {
     await copyTemplatesFolders(feature);
+  }
+
+  for (const feature of features) {
+    await processFeatureChange(feature);
   }
 };
 
