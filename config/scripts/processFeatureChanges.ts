36bc1a6baa5c2289c2d5ca49262d9f270cc08054
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-12-07 09:48:45 -0500

    refactor: simplify spinner message for processing feature changes

--- config/scripts/processFeatureChanges.ts
+++ config/scripts/processFeatureChanges.ts
@@ -1,15 +1,14 @@
 import type Feature from '../types/feature.ts';
 import * as clack from '@clack/prompts';
-import capitalize from '../utils/capitalize.ts';
 import filesFeatureChanges from '../constants/filesFeatureChanges.ts';
 
 const processFeatureChanges = async (feature: Feature) => {
   const spinner = clack.spinner();
-  spinner.start(`Processing ${capitalize(feature)} changes...`);
+  spinner.start(`Processing ${feature} changes...`);
 
   await filesFeatureChanges[feature]();
 
-  spinner.stop(`✔ Processed ${capitalize(feature)} changes.`);
+  spinner.stop(`✔ Processed ${feature} changes.`);
 };
 
 export default processFeatureChanges;
